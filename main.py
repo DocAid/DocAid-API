635381414be3f983e2da1aade726e6f58226fa0f
Rohit Nagraj <rohitnagraj.99@gmail.com>
2019-10-12 06:17:56 +0530

    Added ip:

--- main.py
+++ main.py
@@ -324,8 +324,8 @@ def socket_server():
 
 
 if __name__ == '__main__':
-    # host = "34.93.231.96"
-    host = socket.gethostname()
+    host = "34.93.231.96"
+    # host = socket.gethostname()
     port = 5500
 
     client = socket.socket()
